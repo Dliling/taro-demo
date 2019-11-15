96e49fd6d69a3b944ea7cd3f0d2590070acddf2d
Dlingling <15710055793@163.com>
2019-11-15 10:03:40 +0800

    Add files via upload

---
+++ src/components/list/index.js
@@ -0,0 +1,8 @@
+import Taro, {Component} from '@tarojs/taro';
+import {View} from '@tarojs/components';
+
+export default class List extends Component {
+    render() {
+        return <View className='skeleton-rect'>12345</View>
+    }
+}
